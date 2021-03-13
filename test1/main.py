5a6f8a2e86fbe23ac21b9f39805f0fe98ee454e3
Sergey Shpakov <guttleslug@mail.ru>
2021-03-14 02:50:38 +0300

    Update: Добавлена логика уведомления

--- test1/main.py
+++ test1/main.py
@@ -5,22 +5,37 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QIcon
-from ui_t import Ui_MainWindow              #   Для работы с графическим интерфейсом
+from ui_t import Ui_MainWindow              # Для работы с графическим интерфейсом
+import time                                 # Для работы с временем
+from plyer import notification        # Для работы с уведомлениями
 
-#
+
 class Notifier(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Notifier, self).__init__()    #   Возвращает объект родителя noptyfier и возвращает его конструктор
+        super(Notifier, self).__init__()    # Возвращает объект родителя notifier и возвращает его конструктор
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)               #   Вызываем функцию setupUi чтобы отрисовать интерфейс
+        self.ui.setupUi(self)               # Вызываем функцию setupUi чтобы отрисовать интерфейс
         self.init_UI()
 
-    def init_UI(self):                      #   Функция для работы с графическим интерфейсом
+    def init_UI(self):                      # Функция для работы с графическим интерфейсом
         self.setWindowTitle('NotifyME')
         self.setWindowIcon(QIcon('../загружено.png'))
+        self.ui.plainTextEdit.setPlaceholderText('for example: take your pills')
+        self.ui.pushButton.clicked.connect(self.alert_func)
+
+    def alert_func(self):                    # Функция для обработки сообщений и вывода уведомления
+        input_message = self.ui.plainTextEdit.toPlainText()
+        input_time = self.ui.timeEdit.text()
+
+        notification.notify(                 # Устанавливаем заголовок, текст и картинку уведомлению
+            title='NotifyMe',                # Название уведомления
+            message=input_message,           # Текст
+            app_icon='C:\\Users\\guttl\\Desktop\\curs\\загружено1.ico',     # Картинка в формате ico
+            timeout=10,                      # Время закрепления уведомления
+        )
 
 
-app = QtWidgets.QApplication([])            #   Создаем экземпляр класса QApplication
+app = QtWidgets.QApplication([])            # Создаем экземпляр класса QApplication
 application = Notifier()
 application.show()
 
